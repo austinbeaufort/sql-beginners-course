1272c3da9d9658f59592dbf8a7089f9e28d203fd
austinbeaufort <abeaufort@sescollc.com>
2019-07-18 12:26:23 -0400

    connecting to oracledb with node

--- sqlconnection.js
+++ sqlconnection.js
@@ -1,28 +1,49 @@
+// ------- MYSQL ---------------
+
+// const express = require('express');
+// const app = express();
+// // const oracledb = require('oracledb');
+// const mysql = require('mysql');
+
+// app.get('/', (req, res) => {
+//     res.send('test');
+// })
+
+// let connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "password",
+//     database: "test"
+// })
+
+// connection.connect(() => console.log('connected to db...'));
+
+// connection.query('SELECT * FROM `branch_supplier`', function(error, results, fields) {
+//     console.log(error, results);
+// });
+ 
+// connection.end();
+
+
+// const port = 3000 || process.env.PORT;
+
+// app.listen(port, () => console.log(`listening on port ${port}`));
+
+
+
+
+// ------------------ ORACLE ---------------
+
 const express = require('express');
 const app = express();
-// const oracledb = require('oracledb');
-const mysql = require('mysql');
+const oracledb = require('oracledb');
+const oracleLogin = require('./config');
 
-app.get('/', (req, res) => {
-    res.send('test');
+oracledb.getConnection(oracleLogin, (err, connection) => {
+    console.log(connection, err);
+    connection.execute(
+        'SELECT * FROM forecast_7day WHERE ROWNUM < 100', function(err, result) {
+            console.log(result);
+        }
+    )
 })
-
-let connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "test"
-})
-
-connection.connect(() => console.log('connected to db...'));
-
-connection.query('SELECT * FROM `employee`', function(error, results, fields) {
-    console.log(error, results);
-});
- 
-connection.end();
-
-
-const port = 3000 || process.env.PORT;
-
-app.listen(port, () => console.log(`listening on port ${port}`));
